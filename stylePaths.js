26c9d4fc1c24201ea86055259cbf23d58aa6bb2c
Andy Braren <abraren@redhat.com>
2026-01-22 10:55:19 -0500

    Update vulnerable packages

--- stylePaths.js
+++ stylePaths.js
@@ -10,6 +10,7 @@ module.exports = {
     path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css'),
     path.resolve(__dirname, 'node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css'),
     path.resolve(__dirname, 'node_modules/@patternfly/react-inline-edit-extension/node_modules/@patternfly/react-styles/css'),
-    path.resolve(__dirname, 'node_modules/@patternfly/chatbot/dist/css/main.css')
+    path.resolve(__dirname, 'node_modules/@patternfly/chatbot/dist/css/main.css'),
+    path.resolve(__dirname, 'node_modules/highlight.js/styles')
   ]
 }
